c4d6b2aa58d2a7a16725f3a5e76d602dcade8d9d
Haoran Fang <fanghaoran9054@gmail.com>
2016-08-12 23:39:26 -0700

    Adjusting the background image position for mobile devices

--- static/custom/js/script.js
+++ static/custom/js/script.js
@@ -1,13 +1,14 @@
+$( window ).ready(function() {
+  var windowInnerHeight = $( window ).innerHeight() + "px";
+  var img = new Image();
+  img.src = $('.parallex-section').attr("data-image-src");
 
-function fixHeight() {
-  var container = document.getElementsByClassName('parallex-section');
-  for(i=0; i < container.length; i+=1){
-    container[i].style.height = window.innerHeight + "px";
-  }
-}
 
-window.addEventListener('resize', fixHeight);
-window.onload = function() {
-  fixHeight();
-  $('.parallex-section').parallax();
-}
+  $('.parallex-section')
+    .css("height", windowInnerHeight)
+    .parallax()
+    .css("background-position-x", img.width * 0.4); // 40% offset for background image
+})
+.resize(function() {
+  $('.parallex-section').css("height", $( window ).innerHeight() + "px")
+});
