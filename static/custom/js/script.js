11e3bce50cd98859119e6687327f8becbc806c65
Haoran Fang <fanghaoran9054@gmail.com>
2016-08-14 17:39:15 -0700

    Adjusting the background image position on to fit certain mobile devices

--- static/custom/js/script.js
+++ static/custom/js/script.js
@@ -1,14 +1,13 @@
 $( window ).ready(function() {
-  var windowInnerHeight = $( window ).innerHeight() + "px";
-  var img = new Image();
-  img.src = $('.parallex-section').attr("data-image-src");
+  var windowInnerHeight = $( window ).innerHeight();
+  var windowInnerWidth = $( window ).innerWidth();
 
 
-  $('.parallex-section')
-    .css("height", windowInnerHeight)
+  $('.parallax-section-image')
+    .css("height", windowInnerHeight + "px")
     .parallax()
-    .css("background-position-x", img.width * 0.4); // 40% offset for background image
+    .css("background-position-x", "40%"); // 40% offset for background image
 })
 .resize(function() {
-  $('.parallex-section').css("height", $( window ).innerHeight() + "px")
+  $('.parallax-section-image').css("height", $( window ).innerHeight() + "px")
 });
